fe494aac4ca3639b11353359d1d8b42c7b5f6ed9
Gerard Montemayor <grbmontemayor@gmail.com>
2018-02-25 15:31:44 +0800

    Save posts

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
+saved_bundles = []
+
 @app.route('/sync')
 def sync():
     return 'Calling sync function'
@@ -9,4 +11,5 @@ def sync():
 @app.route('/packet_in', methods=['POST'])
 def packet_in():
     if request.method == 'POST':
+        saved_bundles.append(request.data)
         return request.data
