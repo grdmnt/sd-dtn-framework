dee4cb76b8982e8e2404b01a440d22af3d7fc66d
Gerard Montemayor <grbmontemayor@gmail.com>
2018-02-26 02:24:51 +0800

    add sync and packet_in functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,30 @@
 from flask import Flask
 from flask import request
+import json
 app = Flask(__name__)
+from flask_pymongo import PyMongo
 
-from werkzeug.contrib.cache import SimpleCache
-cache = SimpleCache()
-
-cache.set('saved_bundles', [])
+app.config['MONGO3_URI'] = 'mongodb://sdtn:password@ds247678.mlab.com:47678/sd-dtn-controller'
+app.config['MONGO3_PORT'] = 47678
+app.config['MONGO3_DBNAME'] = 'dbname_three'
+mongo = PyMongo(app, config_prefix='MONGO3')
 
 @app.route('/sync')
 def sync():
-    return 'Calling sync function'
+    ipAddr = request.args.get('ipAddr')
+    flowIds = request.args.get('flowIds')
+    flowIds = flowIds.split(',')
+    flowTables = mongo.db.flow_tables.find_one_or_404({'ipAddr': ipAddr})
+    filtered = []
+    for flow in flowTables["flow_table"]:
+        if flow["flowId"] not in flowIds:
+            filtered.append(flow)
+
+    return str(filtered)
 
 @app.route('/packet_in', methods=['POST'])
 def packet_in():
     if request.method == 'POST':
-        saved_bundles = cache.get('saved_bundles')
-        saved_bundles.append(request.data)
-        print(saved_bundles)
-        cache.set('saved_bundles', saved_bundles)
-        return str(saved_bundles)
+        save = json.loads(request.data)
+        mongo.db.saved_bundles.insert(save)
+        return str(request.data)
