9bd9776bf13451a478a338e244b9576fde4d4105
Gerard Montemayor <grdmnt@grdmnt-PC>
2018-04-20 02:17:45 +0800

    Finish SDN for SDN 2

--- SDDTN.py
+++ SDDTN.py
@@ -1,41 +1,100 @@
 from abc import ABCMeta, abstractmethod 
+import time
 
 class SDDTN:
     __metaclass__ = ABCMeta
 
-    def __init__(self):
-        self.flow_tables = {}
+    def __init__(self, mode):
+        if mode == 2:
+            self.flow_tables = {}
+            self.to_be_deleted = {}
+        elif mode == 1:
+            self.flow_tables = {}
+            self.to_be_deleted = {}
         
     @abstractmethod
-    def boot(self):
+    def boot(self, request):
         pass
     
     @abstractmethod
-    def packet_in(self):
+    def packet_in(self, request):
         pass
 
     @abstractmethod
-    def alive(self):
+    def alive(self, request):
         pass
 
-    def add_flow(self, ip_address, flow):
+    def sim_call(self, request):
+        if request['event_type'] == 0:
+            self.packet_in(request)
+        elif request['event_type'] == 1:
+            self.alive(request)
+        elif request['event_type'] == 2:
+            self.boot(request)
+
+    def install_flow(self, ip_address, flow):
         self.__validate_ip_address(ip_address)
-        self.__validate_flow(flow)
-
+        
+        # Get existing flow tables to check list of existing node ids
+        
         if not ip_address in self.flow_tables.keys():
-            self.flow_tables[ip_address] = []
+            self.flow_tables[ip_address] = {}
+        # Append to specific flow table in DB
+        flow['synced' ] = False
+        flow['expiration'] += time.time()
+        slot = str(flow['priority'])
+        self.flow_tables[ip_address][slot] = flow.copy()
+    
+    def install_flow_to_all(self, flow):
+        for key, value in self.flow_tables.iteritems():
+            self.install_flow(key, flow.copy())
+    
+    def delete_flow_from_all(self, priority):
+        for key, value in self.flow_tables.iteritems():
+            self.delete_flow(key, priority)
+    
+    def delete_flow(self, ip_address, priority):
+        slot = str(priority)
+        del self.flow_tables[ip_address][slot]
+        if not ip_address in self.to_be_deleted.keys():
+            self.to_be_deleted[ip_address] = []
+        if not slot in self.to_be_deleted[ip_address]:
+            self.to_be_deleted[ip_address].append(slot)
 
-        self.flow_tables[ip_address].append(flow)
+    def clean_expired_flows(self, ip_address):
+        for key, value in self.flow_tables[ip_address].iteritems():
+            if value['expiration'] < time.time():
+                self.delete_flow(ip_address, key)
 
     def get_flow_table(self, ip_address):
         try:
             return self.flow_tables[ip_address]
         except KeyError:
             print('IP Address ' + ip_address + ' does not exist in flow tables')
-    
-    def __validate_flow(self, flow):
-        pass
 
+    def sync_flows(self, ip_address):
+        # Query specific flow table and delete expired flows
+        # Return as a JSON response
+        self.clean_expired_flows(ip_address)
+
+        flow_table = self.flow_tables[ip_address]
+
+        response = {}
+        response['install'] = []
+        if not ip_address in self.to_be_deleted.keys():
+            response['delete'] = []
+        else:
+            response['delete'] = self.to_be_deleted[ip_address]
+        self.to_be_deleted[ip_address] = []
+
+        for key, value in flow_table.iteritems():
+            if not value['synced']:
+                flow_to_sync = flow_table[key].copy()
+                flow_to_sync.pop('synced', None)
+                response['install'].append(flow_to_sync)
+                self.flow_tables[ip_address][key]['synced'] = True
+        return response
+    
     def __validate_ip_address(self, ip_address):
         if not type(ip_address) == str:
             raise TypeError('IP Address should be a string')
@@ -49,5 +108,5 @@ class SDDTN:
             if len(section) < 1 or len(section) > 3:
                 raise ValueError('IP Address format is invalid, IP Address given: ' + '.'.join(ip_address))
 
-    
+# Management of Dead Nodes
             
---
+++ fire_detection.py
@@ -0,0 +1,133 @@
+import pprint
+from SDDTN import SDDTN
+
+class FireDetection(SDDTN):
+    def boot(self, request):
+        self.install_flow(request['ip_address']) # lacks flow
+        response = self.sync_flows(request['ip_address'])
+        return response
+    
+    def packet_in(self, request):
+        if (request['data_ave'] > 7):
+            flow = {
+                'priority': 1,
+                'ip_address': '172.24.1.1',
+                'sensor_id': 12,
+                'gt_data_ave': 1,
+                'eq_data_ave': 1,
+                'lt_data_ave': 1,
+                'gt_smallest_val': 1,
+                'eq_smallest_val': 1,
+                'lt_smallest_val': 1,
+                'gt_largest_val': 1,
+                'eq_largest_val': 1,
+                'lt_largest_val': 1,
+                'action': 1,
+                'expiration': 3000
+            }
+            self.install_flow_to_all(flow)
+            self.emergency_sensor = request['sensor_id']
+            print "Entering Emergency Mode"
+        if (request['data_ave'] < 7 and request['sensor_id'] == self.emergency_sensor):
+            print "Exiting Emergency Mode"
+            self.delete_flow_from_all(1)
+        response = self.sync_flows(request['ip_address'])
+        return response
+    
+    def alive(self, request):
+        response = self.sync_flows(request['ip_address'])
+        return response
+
+    def demo(self):
+        flow = {
+            'priority': 1,
+            'ip_address': '172.24.1.1',
+            'sensor_id': 1,
+            'gt_data_ave': 1,
+            'eq_data_ave': 1,
+            'lt_data_ave': 1,
+            'gt_smallest_val': 1,
+            'eq_smallest_val': 1,
+            'lt_smallest_val': 1,
+            'gt_largest_val': 1,
+            'eq_largest_val': 1,
+            'lt_largest_val': 1,
+            'action': 1,
+            'expiration': 3000
+        }
+
+        flow1 = {
+            'priority': 2,
+            'ip_address': '172.24.1.2',
+            'sensor_id': 1,
+            'gt_data_ave': 1,
+            'eq_data_ave': 1,
+            'lt_data_ave': 1,
+            'gt_smallest_val': 1,
+            'eq_smallest_val': 1,
+            'lt_smallest_val': 1,
+            'gt_largest_val': 1,
+            'eq_largest_val': 1,
+            'lt_largest_val': 1,
+            'action': 1,
+            'expiration': 3000
+        }
+
+        flow2 = {
+            'priority': 3,
+            'ip_address': '172.24.1.3',
+            'sensor_id': 1,
+            'gt_data_ave': 1,
+            'eq_data_ave': 1,
+            'lt_data_ave': 1,
+            'gt_smallest_val': 1,
+            'eq_smallest_val': 1,
+            'lt_smallest_val': 1,
+            'gt_largest_val': 1,
+            'eq_largest_val': 1,
+            'lt_largest_val': 1,
+            'action': 1,
+            'expiration': 3000
+        }
+        pp = pprint.PrettyPrinter(indent=1)
+
+        x.install_flow('172.24.1.1', flow)
+        x.install_flow('172.24.1.2', flow)
+        x.install_flow('172.24.1.3', flow)
+        x.delete_flow('172.24.1.3', 1)
+        x.install_flow_to_all( flow1)
+
+        # Start demo for packet in
+        packet = {
+            'event_type': 0,
+            'mobile_id': 0,
+            'ip_address': '172.24.1.1',
+            'sensor_id': 1,
+            'sensor_ip_address': '172.24.1.2',
+            'data_ave': 15.5,
+            'smallest_val' : 4.3,
+            'largest_val': 15.0,
+            'time_received': 123,
+            'datapoints': '1'
+        }
+
+        packet1 = {
+            'event_type': 0,
+            'mobile_id': 0,
+            'ip_address': '172.24.1.1',
+            'sensor_id': 1,
+            'sensor_ip_address': '172.24.1.2',
+            'data_ave': 5.5,
+            'smallest_val' : 4.3,
+            'largest_val': 15.0,
+            'time_received': 123,
+            'datapoints': '1'
+        }
+        x.sim_call(packet)
+        pp.pprint(x.get_flow_table('172.24.1.3'))
+        x.sim_call(packet1)
+        pp.pprint(x.get_flow_table('172.24.1.2'))
+
+x = FireDetection(2)
+x.demo()
+
